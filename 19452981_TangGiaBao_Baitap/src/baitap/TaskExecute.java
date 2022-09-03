60cfed2547e158974b893d392011829c9592d0e6
tanggiabao <tanggiabao.ltp@gmail.com>
2022-09-03 12:27:51 +0700

    first commit

---
+++ 19452981_TangGiaBao_Baitap/src/baitap/TaskExecute.java
@@ -0,0 +1,18 @@
+package baitap;
+
+public class TaskExecute {
+	
+   public static void main(String[] agrs) {
+	   
+	   Runnable r1 = new YourTask("Print Task",20);
+	   Runnable r2 = new YourTask("Distribute Task",23);
+	   Thread t1=new Thread(r1);
+	   Thread t2=new Thread(r2);
+	   t1.start();
+	   t2.start();
+	   
+	   
+	
+}
+
+}
